7816e6a66bd2725e5a1968f5e8a905a1a4f6f6f6
Miles Marillo <mjm03@hampshire.edu>
2019-04-21 15:26:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each do |color_gender_lives, values|
     values.each do |value, array|
       array.each do |name|
-        if hash[name] == NIL
+        if hash[name] == nil
           hash[name] = {}
           hash[name][color_gender_lives] = []
         end
